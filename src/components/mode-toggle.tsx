578d1e6392612042cec2df90289c1da3adaaa3be
tonyphan04 <hoanganhphan2k@gmail.com>
2025-04-11 00:00:54 +1000

    quick button fix

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -17,9 +17,9 @@ export function ModeToggle() {
       onClick={toggleTheme}
     >
       {theme === "dark" ? (
-        <Sun className="h-5 w-5" />
-      ) : (
         <Moon className="h-5 w-5" />
+      ) : (
+        <Sun className="h-5 w-5" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
