03952fedebaa63f086d2b3294d81635612f7c093
tonyphan04 <hoanganhphan2k@gmail.com>
2025-05-16 16:09:17 +1000

    .

--- src/features/banner/banner.test.tsx
+++ src/features/banner/banner.test.tsx
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Banner from "./banner";
