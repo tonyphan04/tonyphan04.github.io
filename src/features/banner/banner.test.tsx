04092ee2640731c3f6c1930933bce37c251e40d2
tonyphan04 <hoanganhphan2k@gmail.com>
2025-05-16 15:22:29 +1000

    update banner

--- src/features/banner/banner.test.tsx
+++ src/features/banner/banner.test.tsx
@@ -3,19 +3,19 @@
  */
 
 import "@testing-library/jest-dom";
-import { screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Banner from "./banner";
 
 describe(Banner, () => {
   it("#projects is visible", () => {
-    //render(<Banner />);
+    render(<Banner />);
     const button = screen.getByRole("button", { name: "View My Work" });
     fireEvent.click(button);
     expect(window.location.hash).toBe("#projects");
   });
 
   it("scroll to contact section when button is clicked", () => {
-    //render(<Banner />);
+    render(<Banner />);
     const link = screen.getByRole("link", { name: "Contact Me" });
     fireEvent.click(link);
     expect(link).toHaveAttribute("href", "/contact");
