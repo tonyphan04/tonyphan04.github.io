800d6cd577580fb4761f1f220ecb95d447bcee0f
tonyphan04 <hoanganhphan2k@gmail.com>
2025-05-16 15:24:01 +1000

    update react

--- src/features/banner/banner.test.tsx
+++ src/features/banner/banner.test.tsx
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Banner from "./banner";
